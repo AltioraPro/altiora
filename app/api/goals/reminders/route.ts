d4a8b8b03a0b8a9f42c597c23cc4c4b54c8723a2
Noa.O <noa.obringer@gmail.com>
2025-07-30 13:58:54 +0200

    Remove: unused NextRequest import

--- app/api/goals/reminders/route.ts
+++ app/api/goals/reminders/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { goalReminderService } from "@/server/services/goal-reminders";
 
 export async function POST() {
