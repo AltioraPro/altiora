314623d5aa26dc50781194272bde21812c9687d4
17Sx <noa.obringer@gmail.com>
2026-01-21 16:10:41 +0100

    Update search-params.ts

--- app/(app)/habits/search-params.ts
+++ app/(app)/habits/search-params.ts
@@ -4,10 +4,8 @@ export const viewModes = ["today", "week", "month"] as const;
 export type ViewMode = (typeof viewModes)[number];
 
 export const habitsSearchParams = {
-    viewMode: parseAsStringEnum(["today", "week", "month"]).withDefault(
-        "today"
-    ),
+  viewMode: parseAsStringEnum(["today", "week", "month"]).withDefault("month"),
 };
 
 export const habitsSearchParamsCache =
-    createSearchParamsCache(habitsSearchParams);
+  createSearchParamsCache(habitsSearchParams);
